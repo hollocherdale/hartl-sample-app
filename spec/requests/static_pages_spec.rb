9d714222049d1da60b90ce9d69ec6963050481d8
hollocherDale <hollocher@gmail.com>
2014-03-28 15:44:34 -0700

    update static pages and tests

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+=begin
+describe "Static pages" do
+  
+  describe "Home pages" do
+
+  	it "should have the content 'Sample App'" do
+  		visit '/static_pages/home'
+  		expect(page).to have_content('Sample App')
+  	end
+  end
+end
+=end
+
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+  end
+end
